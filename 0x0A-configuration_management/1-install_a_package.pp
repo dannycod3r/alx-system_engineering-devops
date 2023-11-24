7d2519d8022bb2856b1482670cce033fb7aa24dd
dannycod3r <oseid7807@gmail.com>
2023-11-24 21:51:37 +0000

    --no-edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,6 @@
 # Configuration to install python flask
 
-# make sure pip is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# install flask here
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
