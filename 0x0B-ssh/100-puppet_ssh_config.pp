dd341612f482621ed71affa0711ff994be0afef5
dannycod3r <oseid7807@gmail.com>
2023-12-10 16:25:42 +0000

    Modify ssh configuration using puppet
    Script enables us to connect to a server without typing a password

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Puppet script that configures ssh
+file_line { 'Turn off password required':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
